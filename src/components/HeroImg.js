238d381751186be60b4878fd17abe9627b90642e
Ray25Soccer <rayirthdasgupta@live.com>
2024-08-05 09:31:16 -0700

    formatted everything
    formatted everything

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,7 +2,7 @@ import "./HeroImgStyles.css";
 
 import React from 'react'
 //import IntroImg from "../assets/intro-img.png"
-import SmallImg from "../assets/ERDM Sports (11).png"
+import SmallImg from "../assets/ERDM Sports (14).png"
 //import {Link} from "react-router-dom"
 
 const HeroImg = () => {
@@ -14,15 +14,19 @@ const HeroImg = () => {
       {/*<div className="content">*/}
       <div className="content">
       <body>
-      <h1>What, Where, When?</h1>
+      <h1>What?</h1>
         <p>
-          Here at the Kick for Cause Sports Camp, our 4 high school trainers teach kids attending the camp the fundamentals of sports like soccer and football. We incoorporate fun activities as well to help kids love sports! We hold two-week camps throughout the summer and hold trainings almost every weekend during the school year! Locations may vary throughout Sammamish, but often sessions will be held at Klahanie Park.
+          Here at the Kick for Cause Sports Camp, our 4 high school trainers teach kids attending the camp the fundamentals of sports like soccer and football. We incoorporate fun activities as well to help kids love sports! Locations may vary throughout Sammamish, but often sessions will be held at Klahanie Park.
           </p>
+          <h1>When?</h1>
+            <p>We hold two-week camps throughout the summer and hold trainings almost every weekend during the school year!</p>
+          <h1>Where?</h1>
+            <p>Locations may vary throughout Sammamish, but often sessions will be held at Klahanie Park.</p>
           <h2>
             Coming Soon...
           </h2>
           <p>
-            Our first camp will be held from August 5th to August 15th on weekdays excluding August 9th! All proceeds of this camp will be donated to UNICEF and Red Cross! The cost for enrollment is $200 for the whole camp!
+            Our first camp will be held from August 7th to August 15th on weekdays excluding August 9th! All proceeds of this camp will be donated to UNICEF and Red Cross! The cost for enrollment is free for the whole camp, but donations are highly appreciated!
           </p>
           {/*<h1>For more information look at our About page!</h1>*/}
   
--- src/routes/About.js
+++ src/routes/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <Navbar/>
     
     <HeroImg2 heading = "ABOUT US." text="We LOVE sports.
-    We are a non-profit! All proceeds go to charity! At the Kick for Cause Sports Camp, your kids get quality training for many sports but mostly soccer. All 4 leaders of the camp are experts at soccer with two of the four, Eric and Ray, playing for Crossfire 09 Academy ECNL, the team that won the ECNL Northwest Conference for the last 4 years in a row! Eric also plays Skyline Football! Another camp leader, Matthias, plays soccer for Western Washington and loves football as well! Finally, Daanish plays soccer for Crossfire 08 ECRL and loves playing spikeball and other sports in his free time! While camp attendees will recieve training for sports, there will also be fun activities at the end!"/>
+    We are a non-profit! All proceeds go to UNICEF and Red Cross! At the Kick for Cause Sports Camp, your kids get quality training for many sports but mostly soccer. While camp attendees will recieve training for sports, there will also be fun activities at the end! Learn about the camp leaders in the 'Meet The Team' section!"/>
     {/*<AboutContent/>*/}
     {/*<Footer/>*/}
   </div>
